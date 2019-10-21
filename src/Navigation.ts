140801fff52ccf4a9af7954333157754cebf4fe4
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-10-21 17:35:34 +0900

    feat: add Tab Navigator

---
+++ src/Navigation.ts
@@ -0,0 +1,15 @@
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {Example} from './Example';
+
+const TabNavigator = createBottomTabNavigator({
+  First: {screen: Example},
+  Second: {screen: Example},
+}, {
+  tabBarOptions: {
+    style: {
+    backgroundColor:'transparent'
+  }
+}});
+
+export const Navigator = createAppContainer(TabNavigator);
