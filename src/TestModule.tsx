01fba6a15ca929f858a739d58de38cfcf9750a81
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-02-02 10:17:20 +0900

    chore: add test branch for toolbarAndroid

--- src/TestModule.tsx
+++ src/TestModule.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+// @ts-ignore
+import ToolbarAndroid from '@react-native-community/toolbar-android';
 
 const styles = StyleSheet.create({
   container: {flex: 1},
+  toolbar: {backgroundColor: '#E9EAED', height: 56},
 });
 export const TestModule = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>This Page will be used to test out components</Text>
-      </View>
+      <ToolbarAndroid
+        title="ToolbarAndroid Example"
+        style={styles.toolbar}
+        actions={[{title: 'Button', show: 'always'}]}
+        // onIconClicked={() => this.setState({message: 'Menu is clicked'})}
+        // onActionSelected={position =>
+        //   this.setState({message: `action ${position} is clicked`})
+        // }
+      />
     </SafeAreaView>
   );
 };
