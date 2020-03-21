085d6a28b91a8a86cd3bf21ead5400fe10a19814
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-03-21 16:07:03 +0900

    feat: test push notification

--- src/TestModule.tsx
+++ src/TestModule.tsx
@@ -1,103 +1,35 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, LayoutAnimation} from 'react-native';
-import {
-  SwipeableFlatList,
-  SwipeableQuickActions,
-  SwipeableQuickActionButton,
-} from 'react-native-swipe-list';
-import {ListItem} from './ListItem';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+
 const styles = StyleSheet.create({
   container: {flex: 1},
 });
 
-const initialData = [...Array(30)].map((_, index) => index);
-
 export const TestModule = () => {
-  const [data, setData] = useState(initialData);
-
+  useEffect(() => {
+    PushNotificationIOS.checkPermissions(result => {
+      console.warn(result);
+    });
+    PushNotificationIOS.requestPermissions();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <SwipeableFlatList
-        data={data.map(index => ({
-          title: 'Hello World',
-          description: 'lorem ipsum',
-          album: index,
-        }))}
-        renderItem={({item}) => <ListItem {...item} />}
-        keyExtractor={index => index.album.toString()}
-        renderLeftActions={({item}) => (
-          <SwipeableQuickActions>
-            <SwipeableQuickActionButton
-              style={{
-                backgroundColor: 'red',
-                padding: 16,
-                width: '100%',
-                height: '100%',
-              }}
-              onPress={() => {
-                LayoutAnimation.configureNext(
-                  LayoutAnimation.Presets.easeInEaseOut,
-                );
-                setData(data.filter(value => value !== item.album));
-              }}
-              text="delete"
-              textStyle={{fontWeight: 'bold', color: 'white'}}
-            />
-          </SwipeableQuickActions>
-        )}
-        renderRightActions={({item}) => (
-          <SwipeableQuickActions>
-            <SwipeableQuickActionButton
-              style={{
-                backgroundColor: 'gray',
-                padding: 16,
-                width: '100%',
-                height: '100%',
-              }}
-              onPress={() => {
-                LayoutAnimation.configureNext(
-                  LayoutAnimation.Presets.easeInEaseOut,
-                );
-                setData(data.filter(value => value !== item.album));
-              }}
-              text="Other"
-              textStyle={{fontWeight: 'bold', color: 'white'}}
-            />
-            <SwipeableQuickActionButton
-              style={{
-                backgroundColor: 'orange',
-                padding: 16,
-                width: '100%',
-                height: '100%',
-              }}
-              onPress={() => {
-                LayoutAnimation.configureNext(
-                  LayoutAnimation.Presets.easeInEaseOut,
-                );
-                setData(data.filter(value => value !== item.album));
-              }}
-              text="Flag"
-              textStyle={{fontWeight: 'bold', color: 'white'}}
-            />
-            <SwipeableQuickActionButton
-              style={{
-                backgroundColor: 'purple',
-                padding: 16,
-                width: '100%',
-                height: '100%',
-              }}
-              onPress={() => {
-                LayoutAnimation.configureNext(
-                  LayoutAnimation.Presets.easeInEaseOut,
-                );
-                setData(data.filter(value => value !== item.album));
-              }}
-              text="Archive"
-              textStyle={{fontWeight: 'bold', color: 'white'}}
-            />
-          </SwipeableQuickActions>
-        )}
-      />
+      <TouchableOpacity
+        onPress={async () => {
+          PushNotificationIOS.scheduleLocalNotification({
+            alertBody: 'Test Local Notification',
+            fireDate: new Date().toISOString(),
+          });
+        }}>
+        <Text>Press Me!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={async () => {
+          console.warn(await PushNotificationIOS.getInitialNotification());
+        }}>
+        <Text>Get getInitialNotification</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
